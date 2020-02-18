0ea502275f92ad4cb86d36275e94fb603a4247f0
Sean Murthy <s.murthy@outlook.com>
2020-02-18 17:14:14 -0500

    Suggested revisions to code organization
    Here is a summary of the changes I suggest through this PR:
    
    - File prologue: Include a high-level problem statement and a URL to the actual problem definition.
    
    - General: use `const` where possible. Please start a SIGCPP thread so we can discus this point further
    
    - L9: Space before `<iostream>`
    
    - L17: variable sum is now declared and initialized at the latest point possible
    
    - L19: use qualified name `std::cout` instead of inserting a qualified name earlier with a `using` declaration. The name `cout` is used only once.
    
    Open questions: The algorithm/strategy is unclear. Specifically, variable names `N` and `M` do not communicate their purpose. These variables names need to be revised to match what they represent. Also, a comment is needed to explain the algorithm/strategy.

--- problem1.cpp
+++ problem1.cpp
@@ -1,31 +1,20 @@
-
 /************************************************
  * Author: Shayon Maramkhah 
- * Description: Project Euler
- * 
- *              Multiples of 3 and 5 
- *              Problem 1, if we list all the natural numbers below 
- *              10 that are multiples of 3 or 5,we get 3, 5, 6 and 9. 
- *              The sum of these multiples is 23. Find the sum of all 
- *              the multiples of 3 or 5 below 1000. 
+ * Description: Problem 1, Project Euler 
+ * https://projecteuler.net/problem=1
  *
+ * Find the sum of all the multiples of 3 or 5 below 1000
  ************************************************/
 
-#include<iostream>
-using std::cout; //Using Declaration 
+#include <iostream>
 
 int main() {
-  
-    unsigned sum = 0;
-    unsigned number = 1000;
+    const unsigned lastNumber = 1000;
     
-    unsigned N = number/3;
-    unsigned M = (number/5)-1;
+    unsigned N = lastNumber/3;
+    unsigned M = (lastNumber/5)-1;
     
-    sum = (3)*(N*(N+1)/2) + (5)*(M*(M+1)/2);
+    unsigned sum = (3)*(N*(N+1)/2) + (5)*(M*(M+1)/2);
     
-    cout<<sum;
-    
-    
-    return 0;
+    std::cout << sum;
 }
